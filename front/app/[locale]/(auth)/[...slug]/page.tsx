83178dd80cba96985e40657664b47b8bb958b66b
Alexandros Theofilis <alekarios@Mac.station>
2026-06-24 00:29:10 +0300

    MasonryArticleList v1

--- front/app/[locale]/(auth)/[...slug]/page.tsx
+++ front/app/[locale]/(auth)/[...slug]/page.tsx
@@ -1,6 +1,7 @@
 import { getDynamicPageBySlug } from '@/lib/strapi/api/dynamic-pages';
 import BlockRenderer from '@/components/BlockRenderer';
 import { notFound } from 'next/navigation';
+import MasonryArticleList from '@/components/strapi-shared/MasonryArticleList';
 
 const DynamicPage = async ({ params }: { params: Promise<{ slug: string[] }> }) => {
   const { slug } = await params;
@@ -9,7 +10,8 @@ const DynamicPage = async ({ params }: { params: Promise<{ slug: string[] }> }) 
 
   return (
     <div>
-      <BlockRenderer blocks={dynamicPage.blocks} />
+      {/* <BlockRenderer blocks={dynamicPage.blocks} /> */}
+      <MasonryArticleList />
     </div>
   );
 };
